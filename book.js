ee6fa59dd29cb65c6366904735da767bcc2203f9
arvidjj <andy.oj14@gmail.com>
2022-11-03 03:04:30 -0300

    Menu's logic

--- book.js
+++ book.js
@@ -22,9 +22,13 @@ function addBookToLibrary(book) {
     console.log("Book added.")
 }
 
+const addBookScreen = document.querySelector('#addBookScreen')
 const booksContainer = document.querySelector('#booksContainer')
 const refreshButton = document.querySelector('#refreshBooksButton')
 refreshButton.addEventListener('click', function () {
+    addButton.removeAttribute('disabled', 'disabled')
+    addBookScreen.style.display = 'none';
+    booksContainer.style.display = 'grid';
     displayBooks();
 })
 
@@ -33,8 +37,11 @@ function displayBooks() {
     myLibrary.forEach((book, index) => {
         const bookCard = document.createElement('div');
         bookCard.classList.add('book')
+        bookCard.setAttribute('id', `${book.title.replace(/\s/g, '').toLowerCase()}`)
         bookCard.innerHTML = `<div class="bookInfo">
-        <h3><strong>${book.title}</strong></h3>
+        <h3><span class="material-symbols-outlined">
+        book
+        </span><strong>${book.title}</strong></h3>
         <p><strong>By:</strong> ${book.author}</p>
         <p><strong>Pages:</strong> ${book.pages}</p></div>
         <button class="readButton notRead">Not read yet</button>
@@ -43,5 +50,13 @@ function displayBooks() {
     })
 }
 
+const addButton = document.querySelector('#addBookButton')
+addButton.addEventListener('click', function (){
+    addButton.setAttribute('disabled', 'disabled')
+    booksContainer.style.display = 'none';
+    addBookScreen.style.display = 'initial';
+})
+
+addBookScreen.style.display = 'none';
 addBookToLibrary(theHobbit)
 addBookToLibrary(elPrincipito)
